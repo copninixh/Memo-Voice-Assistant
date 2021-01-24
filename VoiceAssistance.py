826f5420789a0b316f3987ad27f9d84b2e39d228
Kampanart Chaimooltan <48505@yupparaj.ac.th>
2021-01-24 22:10:24 +0700

    Update Thai Speech

--- VoiceAssistance.py
+++ VoiceAssistance.py
@@ -12,7 +12,7 @@ def voice_command_processor():
         audio = r.listen(source,phrase_time_limit=4)
         text = ''
         try:
-            text =r.recognize_google(audio)
+            text =r.recognize_google(audio , language='th')
         except sr.UnknownValueError as e:
             print(e)
         except sr.RequestError as e:
@@ -23,14 +23,15 @@ def voice_command_processor():
 
 def audio_playback(text):
     filename = "text.mp3"
-    tts = gTTS(text=text, lang='en-us')
+    tts = gTTS(text=text, lang='th')
     tts.save(filename)
     playsound.playsound(filename)
     os.remove(filename)
 
 def execute_voice_command(text):
-    if "hi memo" in  text:
-        audio_playback("i am mem mo voice assistance system")
+    if "สวัสดี" in  text:
+        audio_playback("ว้าว")
+        playsound.playsound('02.mp3')
 
 
 while True:
--- testpy.py
+++ testpy.py
@@ -1,1 +1,46 @@
-print('cop')
+import json
+from gtts import gTTS
+import playsound
+import os
+
+def load_data():
+    try: 
+        f = open("dict.json")
+        json_str = f.read()
+        f.close()
+        dic_data = json.loads(json_str)
+        return dic_data
+    except:
+        dict = {"สวัสดี": "สวัสดีค่ะ", "คุณคือใคร": "ฉันชื่อ 'เก่งแชทบอท'"}
+        js = json.dumps(dict)
+        f = open("dict.json","w")
+        f.write(js)
+        f.close()
+        return dict
+
+def addword(data):
+    print("ไม่พบประโยคนี้ในระบบ คุณต้องการสอนไหม")
+    check = input("Y or N : ")
+    if check == 'Y' or check == 'y':
+        question = input("คำถาม : ")
+        answer = input("คำตอบ : ")
+        data[question] = answer
+        js = json.dumps(data)
+        f = open("dict.json","w")
+        f.write(js)
+        f.close()
+
+def play_sound(data):
+    tts = gTTS(text=data,lang='th')
+    tts.save('sound.mp3')
+    print(data)
+    playsound.playsound('sound.mp3', True)
+    os.system('rm sound.mp3')
+
+while True:
+    data = load_data()
+    text = input("> ")
+    if text in data:
+        play_sound(data[text])
+    else:
+        addword(data)
